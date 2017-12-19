3529a563d28f345d925f13c4e0f4c147827281d1
Kijko <kijkongs@gmail.com>
2017-12-19 11:23:22 +0100

    Clean project

---
+++ kni-webstore/src/main/java/kni/webstore/KniWebstoreApplication.java
@@ -0,0 +1,12 @@
+package kni.webstore;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KniWebstoreApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KniWebstoreApplication.class, args);
+	}
+}
